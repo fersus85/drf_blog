9f5904abbf31431985c53003c6b4d9d30bce11aa
fersus <deriabin_85@mail.ru>
2023-06-22 10:51:52 +0300

    add authorization and authentication

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from .permissions import IsAuthorOrReadOnly
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, CreateAPIView
 from posts.models import Post
 from posts.serializers import PostSerializer
@@ -5,10 +6,12 @@ from posts.serializers import PostSerializer
 
 # Create your views here.
 class PostList(CreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
